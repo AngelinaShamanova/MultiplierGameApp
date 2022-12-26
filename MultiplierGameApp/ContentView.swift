0ca5a67201369ba17a97f3826ae53d575e76a115
AngelinaShamanova <shamanovaaaa@yandex.ru>
2022-12-26 22:14:10 +0100

    Initial Commit

---
+++ MultiplierGameApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MultiplierGameApp
+//
+//  Created by Ангелина Шаманова on 26.12.22..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MultiplierGameApp/MultiplierGameAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplierGameAppApp.swift
+//  MultiplierGameApp
+//
+//  Created by Ангелина Шаманова on 26.12.22..
+//
+
+import SwiftUI
+
+@main
+struct MultiplierGameAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
